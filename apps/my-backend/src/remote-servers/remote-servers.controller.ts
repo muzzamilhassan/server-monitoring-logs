90e865d9a0179b9f1ba8f0cf655053d21bd6fe4d
Muzzamil Hassan <muzzamilhassan302@gmail.com>
2026-05-13 12:07:29 +0500

    feat: add authentication guard to create remote server endpoint

--- apps/my-backend/src/remote-servers/remote-servers.controller.ts
+++ apps/my-backend/src/remote-servers/remote-servers.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { RemoteServersService } from './remote-servers.service';
 import { CreateRemoteServerDto } from './dto/create-remote-server.dto';
 import { UpdateRemoteServerDto } from './dto/update-remote-server.dto';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import type{ UserDto } from 'src/users/dto/user.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('remote-servers')
 export class RemoteServersController {
   constructor(private readonly remoteServersService: RemoteServersService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createRemoteServerDto: CreateRemoteServerDto, @CurrentUser() user: UserDto ) {
     console.log(user)
     return this.remoteServersService.create(createRemoteServerDto);
